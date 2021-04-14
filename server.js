0fb61deb1db2d31b16b354c38fd63f71cc6fadeb
Nathan <nathanbommezijn@gmail.com>
2021-04-14 10:15:08 +0200

    :poop: Write example db insert

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 import express from 'express';
 import compression from 'compression';
+import dotenv from 'dotenv';
 import {fileURLToPath} from 'url';
+import firebase from '@firebase/app';
+import '@firebase/firestore';
+
+dotenv.config();
 const APP = express();
 const PORT = process.env.PORT || 8080;
 const __dirname = fileURLToPath(import.meta.url);
+
+const firebaseConfig = {
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCEKT,
+};
+
+firebase.default.initializeApp(firebaseConfig);
+const db = firebase.default.firestore();
+// const db = firebase.firestore();
 
 APP.use(compression({level: 6}));
 APP.use(express.static(__dirname + '/public'));
@@ -11,10 +27,31 @@ APP.set('view engine', 'ejs');
 APP.set('views', 'views');
 
 APP.get('/', ( request, result ) => {
+  insertUserDb('nathan', 'bommezijn', 1997);
   result.render('index', {
     title: 'Markeer',
   });
 });
+
+/**
+ * @description Add user to firestore
+ * @param {*} first firstname
+ * @param {*} last lastname
+ * @param {*} born YearOfBirth
+ */
+function insertUserDb(first, last, born) {
+  db.collection('users').add({
+    first: first,
+    last: last,
+    born: born,
+  })
+    .then((docRef) => {
+      console.log(`Doc written with ID: ${docRef.id}`);
+    })
+    .catch((error) => {
+      console.error(`Error adding documents: ${error}`);
+    });
+}
 
 APP.listen(PORT, () => {
   console.log(`Listening to port http://localhost:${PORT}`);
