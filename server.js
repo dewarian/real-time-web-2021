2ead5af8b4db3fa5c8a477f3ee25445818eccedb
dewarian <nathanbommezijn@gmail.com>
2021-04-20 01:35:36 +0200

    :memo: reorganize todo

--- server.js
+++ server.js
@@ -4,14 +4,16 @@
  * TODO Connect Database to React frontend
  * TODO CRUD functionality, Save document pushes content to firestore.
  * ---
+ * ! SHOULD
+ * TODO Describe socket.io events and their use
+ * What does each event do and what is it used for?
  * TODO Implement insertion of data attributes in the rendered HTML.
  * With data attr I can style the HTML or show HTML tags in the rendereed window.
  * TODO Describe Firestore / lowdb
  * What is it used for and what does it do in this project
  * TODO Update Repo description
+ * ! COULD
  * TODO Split code further up
- * TODO Describe socket.io events and their use
- * What does each event do and what is it used for?
  */
 /* eslint-enable max-len */
 
