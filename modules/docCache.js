a6db9c18fd14822632d91851150cf7290f845250
dewarian <nathanbommezijn@gmail.com>
2021-04-20 11:45:36 +0200

    :memo: Fix credits 💳

--- modules/docCache.js
+++ modules/docCache.js
@@ -1,10 +1,17 @@
+/**
+ * @author Sjagoori
+ * @link github.com/sjagoori
+ * @description Storage implementation with lowdb by Sjagoori.
+ * Stored data is a keyValue pair stored in json.
+ */
+
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const adapter = new FileSync('docCache.json');
 const db = low(adapter);
 
 /**
- * Function set key:value pair
+ * @description Function set key:value pair to json file
  * @param {String} room - key
  * @param {Object} value - value
  */
@@ -22,7 +29,7 @@ exports.getCache = (room) => {
 };
 
 /**
- * Function returns all entries
+ * @return {Function} getState function form lowdb.
  */
 exports.getAllCaches = () => {
   return db.getState();
