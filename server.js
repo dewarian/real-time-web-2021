cb6cb47bebe403e409f52b26b0387a7299d71e65
Nathan <nathanbommezijn@gmail.com>
2021-04-27 12:05:19 +0200

    :wastebasket: Cleanse old code

--- server.js
+++ server.js
@@ -1,45 +1,30 @@
 require('dotenv').config();
+const compression = require('compression');
 const express = require('express');
 const APP = express();
-const compression = require('compression');
-
 const httpServer = require('http').createServer(APP);
 const io = require('socket.io')(httpServer);
-
 const PORT = process.env.PORT || 8080;
 
-// Basic Express setup
-APP.use(compression({level: 6}));
-APP.use(express.static(__dirname + '/public'));
-APP.set('view engine', 'ejs');
-APP.use(express.json());
-APP.use(express.urlencoded({extended: false}));
-APP.set('views', 'views');
-
+// Socket.io setup, imports events module and use those in io.on
 const events = require('./modules/ioEvents');
-// Socket IO connections
 io.on('connection', (socket) => {
   events.ioEvents(socket, io);
 });
 
+// Basic Express setup with compression and EJS
+APP.use(express.json());
+APP.use(express.urlencoded({extended: false}));
+APP.use(express.static(__dirname + '/public'));
+APP.use(compression({level: 6}));
+APP.set('view engine', 'ejs');
+APP.set('views', 'views');
 
-APP.get('/doc/:room', (req, res)=>{
-  // dont forget to include document contents here, fetch em from firebase
-  const prep = req.params.room;
-  console.log(prep);
-  return res.render('docViewer', {data: 'templateText', room: prep});
-});
-
-APP.post('/doc/handleDoc', (req, res) => {
-  const room = req.body.docName;
-  // console.log(room);
-  res.redirect('/doc/' + room);
-});
-
-APP.get('/', (request, result) => {
-  // userService.insertUserDb(newUser);
-  return result.render('homepage');
-});
+// Route definition of the application
+const indexRoute = require('./routes/indexRouter.js');
+const docRoute = require('./routes/docRouter.js');
+APP.use('/', indexRoute);
+APP.use('/', docRoute);
 
 httpServer.listen(PORT, () => {
   console.log(`Listening to port http://localhost:${PORT}`);
